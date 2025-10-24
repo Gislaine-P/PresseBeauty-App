099a2aa358f383ff975782206a337775bf0e1c05
thais <tha.caamano@duocuc.cl>
2025-10-24 17:24:26 -0300

    login semicreado

---
+++ app/src/main/java/com/example/pressbeauty/model/LoginUI.kt
@@ -0,0 +1,8 @@
+package com.example.pressbeauty.model
+
+import android.net.Uri
+data class LoginUI (
+    val nombre : String,
+    val clave : String
+
+)
---
+++ app/src/main/java/com/example/pressbeauty/viewmodel/LoginViewModel.kt
@@ -0,0 +1,11 @@
+package com.example.pressbeauty.viewmodel
+
+import com.example.pressbeauty.model.LoginUI
+import com.example.pressbeauty.model.UsuarioUI
+import kotlinx.coroutines.flow.MutableStateFlow
+
+class LoginViewModel (
+
+){
+     private val _estado2 = MutableStateFlow(LoginUI(nombre = "", clave = ""))
+}
